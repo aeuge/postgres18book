18a48d8e96ec882af6a41b15fce55b0395c95572
Aristov Eugene <ifti@yandex.ru>
2026-04-02 13:29:03 +0700

    Update wal-g.sql

--- scripts/19/wal-g.sql
+++ scripts/19/wal-g.sql
@@ -1,20 +1,9 @@
--- postgres15
-gcloud beta compute --project=celtic-house-266612 instances create postgres --zone=us-central1-a --machine-type=e2-medium --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=933982307116-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image-family=ubuntu-2004-lts --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-ssd --boot-disk-device-name=postgres --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
-
-gcloud compute ssh postgres
-
-sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y -q && sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt -y install postgresql-15 tree
-sudo pg_lsclusters
-
--- wal-g v1.1.1 - 2022
--- wal-g v2.0.1 
+-- wal-g v3.0.8
 -- https://github.com/wal-g/wal-g
--- install
+sudo pg_lsclusters
+sudo pg_dropcluster 18 main2 --stop
 
--- https://github.com/wal-g/wal-g/releases
--- !!! wget https://github.com/wal-g/wal-g/releases/download/v2.0.1/wal-g-fdb-ubuntu-20.04-amd64.tar.gz
--- sudo rm /usr/local/bin/wal-g
-wget https://github.com/wal-g/wal-g/releases/download/v2.0.1/wal-g-pg-ubuntu-20.04-amd64.tar.gz && tar -zxvf wal-g-pg-ubuntu-20.04-amd64.tar.gz && sudo mv wal-g-pg-ubuntu-20.04-amd64 /usr/local/bin/wal-g
+wget https://github.com/wal-g/wal-g/releases/download/v3.0.8/wal-g-pg-24.04-amd64.tar.gz && tar -zxvf wal-g-pg-24.04-amd64.tar.gz && sudo mv wal-g-pg-24.04-amd64 /usr/local/bin/wal-g
 
 sudo ls -l /usr/local/bin/wal-g
 
@@ -26,56 +15,61 @@ nano ~/.walg.json
 
 -- https://github.com/wal-g/wal-g/blob/master/docs/PostgreSQL.md
 -- https://github.com/wal-g/wal-g/blob/master/docs/STORAGES.md
-
+-- в 3- версии параметр другой     "PGHOST": "/var/run/postgresql/.s.PGSQL.5432"
 {
     "WALG_FILE_PREFIX": "/home/backups",
     "WALG_COMPRESSION_METHOD": "brotli",
     "WALG_DELTA_MAX_STEPS": "5",
-    "PGDATA": "/var/lib/postgresql/15/main",
-    "PGHOST": "/var/run/postgresql/.s.PGSQL.5432"
+    "PGDATA": "/var/lib/postgresql/18/main",
+    "PGHOST": "/var/run/postgresql"
 }
 
 -- опция для дебага
 --     "WALG_LOG_LEVEL": "DEVEL"
 
-mkdir /var/lib/postgresql/15/main/log
+mkdir /var/lib/postgresql/18/main/log
 
--- postgresql.conf
-echo "wal_level=replica" >> /var/lib/postgresql/15/main/postgresql.auto.conf
-echo "archive_mode=on" >> /var/lib/postgresql/15/main/postgresql.auto.conf
-echo "archive_command='wal-g wal-push \"%p\" >> /var/lib/postgresql/15/main/log/archive_command.log 2>&1' " >> /var/lib/postgresql/15/main/postgresql.auto.conf 
-echo "archive_timeout=60" >> /var/lib/postgresql/15/main/postgresql.auto.conf 
-echo "restore_command='wal-g wal-fetch \"%f\" \"%p\" >> /var/lib/postgresql/15/main/log/restore_command.log 2>&1' " >> /var/lib/postgresql/15/main/postgresql.auto.conf
+-- postgresql.con
+echo "wal_level=replica" >> /var/lib/postgresql/18/main/postgresql.auto.conf
+echo "archive_mode=on" >> /var/lib/postgresql/18/main/postgresql.auto.conf
+echo "archive_command='wal-g wal-push \"%p\" >> /var/lib/postgresql/18/main/log/archive_command.log 2>&1' " >> /var/lib/postgresql/18/main/postgresql.auto.conf 
+echo "archive_timeout=60" >> /var/lib/postgresql/18/main/postgresql.auto.conf 
+echo "restore_command='wal-g wal-fetch \"%f\" \"%p\" >> /var/lib/postgresql/18/main/log/restore_command.log 2>&1' " >> /var/lib/postgresql/18/main/postgresql.auto.conf
 
-cat ~/15/main/postgresql.auto.conf
+cat ~/18/main/postgresql.auto.conf
 
 -- Перезапускаем кластер PostgreSQL
-pg_ctlcluster 15 main stop
-pg_ctlcluster 15 main start
+pg_ctlcluster 18 main stop && pg_ctlcluster 18 main start
 cd /home/backups
 
 -- Создадим новую базу данных
-psql -c "CREATE DATABASE otus;"
+psql -c "CREATE DATABASE testw;"
 
 -- Таблицу в этой базе данных и заполним ее тестовыми данными
-psql otus -c "create table test(i int);"
-psql otus -c "insert into test values (10), (20), (30);"
-psql otus -c "select * from test;"
+psql testw -c "create table test(i int);"
+psql testw -c "insert into test values (10), (20), (30);"
+psql testw -c "select * from test;"
 
 -- бэкап
-wal-g backup-push /var/lib/postgresql/15/main 
+wal-g backup-push /var/lib/postgresql/18/main 
 
-cat /var/log/postgresql/postgresql-15-main.log
-cat /var/lib/postgresql/15/main/log/archive_command.log
+cat /var/log/postgresql/postgresql-18-main.log
+cat /var/lib/postgresql/18/main/log/archive_command.log
+
+PGUSER=backup wal-g backup-push /var/lib/postgresql/18/main
+PGDATABASE=postgres PGUSER=backup wal-g backup-push /var/lib/postgresql/18/main
+
+-- show hba_file;
+-- nano /etc/postgresql/18/main/pg_hba.conf
 
 
 
 wal-g backup-list
 
-psql otus -c "UPDATE test SET i = 3 WHERE i = 30"
+psql testw -c "UPDATE test SET i = 3 WHERE i = 30"
 
 -- make delta
-wal-g backup-push /var/lib/postgresql/15/main
+wal-g backup-push /var/lib/postgresql/18/main
 
 wal-g backup-list
 
@@ -83,18 +77,18 @@ cd /home/backups
 tree
 
 -- restore 
-pg_createcluster 15 main2
-rm -rf /var/lib/postgresql/15/main2
+pg_createcluster 18 main2
+rm -rf /var/lib/postgresql/18/main2
 
-wal-g backup-fetch /var/lib/postgresql/15/main2 LATEST
+wal-g backup-fetch /var/lib/postgresql/18/main2 LATEST
 
 
 -- сделаем файл для восстановления из архивов wal
-touch "/var/lib/postgresql/15/main2/recovery.signal"
+touch "/var/lib/postgresql/18/main2/recovery.signal"
 
-pg_ctlcluster 15 main2 start
+pg_ctlcluster 18 main2 start
 
-psql -p 5433 otus -c "select * from test;"
+psql -p 5433 testw -c "select * from test;"
 
 ls -la /home/backups/wal_005
 
@@ -107,31 +101,29 @@ nano ~/.walg.json
     "GOOGLE_APPLICATION_CREDENTIALS" : "/var/lib/postgresql/celtic-house-266612-65d95e64c26a.json",
     "WALG_COMPRESSION_METHOD": "brotli",
     "WALG_DELTA_MAX_STEPS": "5",
-    "PGDATA": "/var/lib/postgresql/15/main",
-    "PGHOST": "/var/run/postgresql/.s.PGSQL.5432"
+    "PGDATA": "/var/lib/postgresql/18/main",
+    "PGHOST": "/var/run/postgresql"
 }
 
 gcloud compute instances list
 
-scp /mnt/d/download/celtic-house-266612-65d95e64c26a.json aeugene@34.66.129.195:/home/aeugene/
+scp /mnt/d/download/celtic-house-266612-65d95e64c26a.json aeugene@34.136.57.48:/home/aeugene/
 cp /home/aeugene/celtic-house-266612-65d95e64c26a.json /var/lib/postgresql/celtic-house-266612-65d95e64c26a.json
 chown postgres:postgres /var/lib/postgresql/celtic-house-266612-65d95e64c26a.json
 
-wal-g backup-push /var/lib/postgresql/15/main
+wal-g backup-push /var/lib/postgresql/18/main
 
 wal-g backup-list
 
 -- восстановимся
-pg_ctlcluster 15 main2 stop
+pg_ctlcluster 18 main2 stop
 
-rm -rf /var/lib/postgresql/15/main2
+rm -rf /var/lib/postgresql/18/main2
 
-wal-g backup-fetch /var/lib/postgresql/15/main2 LATEST
+wal-g backup-fetch /var/lib/postgresql/18/main2 LATEST
 
-touch "/var/lib/postgresql/15/main2/recovery.signal"
-
-
-pg_ctlcluster 15 main2 start
+touch "/var/lib/postgresql/18/main2/recovery.signal"
 
 
-gcloud compute instances delete postgres
+pg_ctlcluster 18 main2 start
+
